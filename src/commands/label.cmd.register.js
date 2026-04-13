2c463d12798fb8b534fe4686e7c6643bd22a0493
Chad Hubbell <chadhub5@users.noreply.github.com>
2026-04-13 10:53:44 +0800

    add jsdoc comment to register function in label cmd register

--- src/commands/label.cmd.register.js
+++ src/commands/label.cmd.register.js
@@ -1,5 +1,10 @@
 const { registerLabelCommands } = require('./label.cmd');
 
+/**
+ * Registers all label-related subcommands onto the CLI program.
+ *
+ * @param {import('commander').Command} program - The root commander program instance.
+ */
 function register(program) {
   registerLabelCommands(program);
 }
